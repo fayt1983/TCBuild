c9de3df3ee2480a099da67adf9d901fbef775445
Wu Xun <wuxun1983@163.com>
2018-04-19 21:27:28 +0800

    add
    add

--- Assets/Editor/MyBuilder.cs
+++ Assets/Editor/MyBuilder.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
+using UnityEditor;
 
-public class MyBuilder : MonoBehaviour {
+public class MyBuilder  {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	static List<string> levels = new List<string>();  
+	//static string keystoreFile = @"D:\keystore.txt";  
+
+	public static void BuildAndroid() {  
+
+
+		  
+
+		foreach ( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ) {  
+			if ( !scene.enabled ) continue;  
+			levels.Add( scene.path );  
+		}  
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);  
+		string res = BuildPipeline.BuildPlayer( levels.ToArray(), "android.apk", BuildTarget.Android, BuildOptions.None );  
+		if (res.Length > 0)  
+			throw new Exception("BuildPlayer failure: " + res);  
+	}  
 }
